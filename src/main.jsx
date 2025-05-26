fb85d154dfaa0afe56ba5e69386f128b3bda7d41
OthiRaja <othiraja5677@gmail.com>
2025-05-27 01:05:08 +0530

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { StrictMode, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client';
 import  CustomNavbar from './components/Nav.jsx'
-import Home from './components/home.jsx'
+import Home from './components/Home.jsx'
 import { ToastContainer } from 'react-bootstrap'
 import About from './components/about/About.jsx'
 import LoadingSpinner from './components/Loader.jsx';
